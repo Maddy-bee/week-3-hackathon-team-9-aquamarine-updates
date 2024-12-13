a2c653234d304e090cc2cd7846a4ea8926c1e783
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2024-12-13 09:21:25 +0000

    Search and text input functionality for API call
    Now handled by UI.

--- fetch.js
+++ fetch.js
@@ -1,27 +1,41 @@
 
-// variables to inject interpolation into the api fetch request
-let artistName = "Beyonce";
-let songTitle = "Diva";
-
 async function fetchData() {
-    try {
-      let response = await fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`, {
-        headers: {
-        "Content-Type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      // Handle your data
-      console.log(data.lyrics); // .lyrics to output text only
-      return data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      // Handle any errors
+  try {
+    let response = await fetch(`https://api.lyrics.ovh/v1/${artistName.value}/${songTitle.value}`, {
+      headers: {
+      "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const data = await response.json();
+    // Handle your data
+    // console.log(data.lyrics); // Test .lyrics to output text only
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    // Handle any errors
   }
+}
 
-  fetchData();
+// Button
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", getAndDisplayLyrics);
+
+// Text Input - Full element, requires .value for grabbing the text (ie artistName.value)
+const artistName = document.querySelector(".artist-input"); 
+const songTitle = document.querySelector(".title-input"); 
+
+// waits for the fetch to finish, then runs the displayLyrics function
+async function getAndDisplayLyrics() {
+let theLyrics = await fetchData();
+displayLyrics(theLyrics);
+}
+
+// grabs the lyrics element, and replaces the textContent with the new lyrics
+function displayLyrics(lyricString) {
+let lyrics = document.getElementById("lyrics");
+lyrics.textContent = lyricString.lyrics;
+}
 
